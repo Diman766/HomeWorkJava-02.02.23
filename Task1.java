da87071f4650efc8b0fb5defa22514dc5a09b3e4
Diman76 <Dmitiy_yar@mail.ru>
2023-02-06 09:46:03 +0300

    fixed task1

--- Task1.java
+++ Task1.java
@@ -1,7 +1,5 @@
 import java.util.LinkedList;
 import java.util.Queue;
-// import java.util.ArrayList;
-// import java.util.Arrays;
 
 public class Task1 {
 
@@ -15,8 +13,8 @@ public class Task1 {
             labirint[j][0] = -1;
             labirint[j][10] = -1;
         }
-        // labirint[1][4] = -1;
-        // labirint[2][4] = -1;
+        labirint[1][4] = -1;
+        labirint[2][4] = -1;
         int x = 1;
         int y = 1;
         labirint[x][y] = 1;
@@ -49,48 +47,27 @@ public class Task1 {
 
         x = 7;
         y = 9;
-        // labirint[x][y] = -2;
         while (labirint[x][y] > 1){
-            if (labirint[x][y] == labirint[x - 1][y] - 1) {
+            if (labirint[x][y] == labirint[x - 1][y] + 1) {
                 labirint[x][y] = -2;
                 x = x - 1;
-                print(labirint);
             } 
-            if (labirint[x][y] == labirint[x][y + 1] - 1) {
+            else if (labirint[x][y] == labirint[x][y + 1] + 1) {
                 labirint[x][y] = -2;
                 y = y + 1;
-                print(labirint);
             } 
-            if (labirint[x][y] == labirint[x + 1][y] - 1) {
+            else if (labirint[x][y] == labirint[x + 1][y] + 1) {
                 labirint[x][y] = -2;
                 x = x + 1;
-                print(labirint);
             } 
-            if (labirint[x][y] == labirint[x][y - 1] - 1) {
+            else if (labirint[x][y] == labirint[x][y - 1] + 1) {
                 labirint[x][y] = -2;
                 y = y - 1;
-                print(labirint);
             }
-            
-            // if (labirint[x][y] == labirint[x - 1][y] - 1) {
-            //     labirint[x][y] = -2;
-            //     x = x - 1;
-            //     print(labirint);
-            // } else if (labirint[x][y] == labirint[x][y + 1] - 1) {
-            //     labirint[x][y] = -2;
-            //     y = y + 1;
-            //     print(labirint);
-            // } else if (labirint[x][y] == labirint[x + 1][y] - 1) {
-            //     labirint[x][y] = -2;
-            //     x = x + 1;
-            //     print(labirint);
-            // } else if (labirint[x][y] == labirint[x][y - 1] - 1) {
-            //     labirint[x][y] = -2;
-            //     y = y - 1;
-            //     print(labirint);
-            // }  
+             
         }
-        // print(labirint);
+        labirint[x][y] = -2;
+        print(labirint);
     }
 
     public static void print(int[][] puzzle) {
@@ -103,47 +80,5 @@ public class Task1 {
         System.out.println();
     }
 
-    // public static String str(int x, int y) {
-    // String str = Integer.toString(x);
-    // str += Integer.toString(x);
-    // return str;
-    // }
-
-    // public static int[] coordinates(String str) {
-    // int[] x = new int[2];
-    // x[0] = (Integer.parseInt(str) / 10);
-    // x[1] = (Integer.parseInt(str) % 10);
-    // return x;
-    // }
-
-    // public void findPath(int x,int y) {
-    // ArrayList<Pair<Integer,Integer>> queue = new ArrayList<>();
-    // queue.add(new Pair<Integer,Integer>());
-    // int[][] mas;
-    // mas[x][y] = 1;
-    // while (queue.size() > 0) {
-    // Pair<Integer,Integer> cur = queue.remove(queue.size() - 1);
-    // int x = cur.x;
-    // int y = cur.y;
-    // //
-    // if (x < width - 1 && mas[x + 1][y] == 0) {
-    // queue.add(new Pair<Integer,Integer>(x+1, y);
-    // mas[x+1][y] = 1;
-    // }
-    // if (x > 0 && mas[x - 1][y] == 0) {
-    // queue.add(new Pair<Integer,Integer>(x-1,y);
-    // mas[x-1][y] = 1;
-    // }
-    // if (y < height - 1 && mas[x][y+1] == 0) {
-    // queue.add(new Pair<Integer,Integer>(x, y + 1);
-    // mas[x][y+1] = 1;
-    // }
-    // if (y > 0 && mas[x][y-1] == 0) {
-    // queue.add(new Pair<Integer,Integer>(x, y - 1);
-    // mas[x][y-1] = 1;
-    // }
-    // //
-    // }
-    // }
 
 }
